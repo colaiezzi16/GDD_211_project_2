3277f9914391e65cbe73c8e4c752868f747c5e14
hexblade12 <78049247+hexblade12@users.noreply.github.com>
2021-05-02 15:28:00 -0400

    CutScene

---
+++ Assets/Scripts/TimelineLoadNext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimelineLoadNext : MonoBehaviour
+{
+    
+    
+    //will activate script on the timeline
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
